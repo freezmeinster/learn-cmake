395ac17f5b0a047da8f1fa99b2bdd75c43914e4b
bramandityo <freez.meinster@gmail.com>
2021-06-10 02:34:15 +0700

    ya sudah lah

--- tutorial.cpp
+++ tutorial.cpp
@@ -1,10 +1,12 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
+#include <iostream>
 
 int
 main(int argc, char *argv[])
 {
     wchar_t *program = Py_DecodeLocale(argv[0], NULL);
+    std::cout << program << std::endl;
     if (program == NULL) {
         fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
         exit(1);
